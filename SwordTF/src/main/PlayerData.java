a6a0be3b23e5d39779e451854e9f8e8219159890
Clayton Hawken <claytonhawken@gmail.com>
2014-07-09 18:42:58 -0500

    Working Scheduler

--- SwordTF/src/main/PlayerData.java
+++ SwordTF/src/main/PlayerData.java
@@ -10,34 +10,37 @@ import org.bukkit.scheduler.BukkitScheduler;
 public class PlayerData {
 	int energy, taskID = -1;
 	BukkitScheduler scheduler;
-	EnergyRunnable task;
 	Plugin plugin;
-	
+
 	public PlayerData(Plugin plugin){
 		scheduler = Bukkit.getServer().getScheduler();
 		energy = 100;
 		this.plugin = plugin;
 	}
-	
+
 	public boolean swingReady(){
 		return energy > Main.ENERGY_PER_SWING;
 	}
-	
+
 	public void swingPerformed(){
 		energy -= Main.ENERGY_PER_SWING;
 		if(taskID != -1){
 			scheduler.cancelTask(taskID);
 		}
-		//taskID = scheduler.scheduleSyncRepeatingTask(plugin ,task, 5, 5);
+		taskID = scheduler.scheduleSyncRepeatingTask(plugin ,new Runnable(){
+			
+			@Override
+			public void run() {
+				if(energy>100){
+					scheduler.cancelTask(taskID);
+				}else{
+					energy+=5;
+				}
+			}
+
+		}, 5, 5);
 	}
 
-	public class EnergyRunnable implements Runnable{
 
-		@Override
-		public void run() {
-			energy+=10;
-		}
-		
-	}
-	
+
 }
