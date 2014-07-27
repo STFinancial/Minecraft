5e70394b0e4aa4ea00afe1a23e792626bd60d2cd
qsik <qsik777@gmail.com>
2014-07-27 16:42:59 -0500

    Friendly fire finished in Event Manager

--- STFArena/src/main/EventManager.java
+++ STFArena/src/main/EventManager.java
@@ -10,7 +10,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.player.PlayerAnimationEvent;
