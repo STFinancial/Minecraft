05038c4ab96957cab417343e10eacbc0a2280a4f
Clayton Hawken <Claytonhawken@gmail.com>
2014-08-04 15:31:52 -0500

    Removed Scoreboard

--- SwordTF/src/main/SwordData.java
+++ SwordTF/src/main/SwordData.java
@@ -5,10 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitScheduler;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.Scoreboard;
 
 public class SwordData implements Runnable {
 	public static final int ENERGY_PER_SWING = 40;
@@ -22,10 +18,6 @@ public class SwordData implements Runnable {
 	private final BukkitScheduler scheduler;
 	private final SwordMain plugin;
 	boolean damageReady = false;
-	private final Scoreboard board = Bukkit.getScoreboardManager()
-			.getNewScoreboard();
-	private Objective objective;
-	private Score score;
 	private final Player player;
 	boolean criticalBlock = false;
 
@@ -33,11 +25,6 @@ public class SwordData implements Runnable {
 		scheduler = Bukkit.getServer().getScheduler();
 		this.plugin = plugin;
 		this.player = player;
-		player.setScoreboard(board);
-		objective = board.registerNewObjective(" ", "dummy");
-		score = objective.getScore("Energy:");
-		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-		score.setScore(energy);
 	}
 
 	public int getEnergy() {
@@ -62,7 +49,6 @@ public class SwordData implements Runnable {
 
 	public void swingPerformed() {
 		energy -= ENERGY_PER_SWING;
-		score.setScore(energy);
 		if (taskID != -1) {
 			scheduler.cancelTask(taskID);
 		}
@@ -107,7 +93,6 @@ public class SwordData implements Runnable {
 			taskID = -1;
 		} else {
 			energy += TICKS_PER_UPDATE * ENERGY_PER_TICK;
-			score.setScore(energy);
 			if (energy >= ENERGY_PER_SWING - TICKS_PER_UPDATE) {
 				if (player.hasPotionEffect(PotionEffectType.SLOW_DIGGING)) 
 					player.removePotionEffect(PotionEffectType.SLOW_DIGGING);
@@ -122,13 +107,11 @@ public class SwordData implements Runnable {
 			scheduler.cancelTask(blockID);
 		}
 
-		//player.sendMessage("Why the fuck do you still showup");
 		criticalBlock = true;
 		blockID = scheduler.scheduleSyncDelayedTask(plugin, new Runnable(){
 
 			@Override
 			public void run() {
-				//player.sendMessage("test, block worn off");
 				criticalBlock = false;
 				blockID = -1;
 			}
@@ -139,7 +122,6 @@ public class SwordData implements Runnable {
 
 	public void changeEnergy(int amount) {
 		energy += amount;
-		score.setScore(energy);
 	}
 
 
